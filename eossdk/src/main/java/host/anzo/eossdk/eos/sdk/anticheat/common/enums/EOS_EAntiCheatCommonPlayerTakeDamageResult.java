8784cb5935e320c8db604acc8a13514f96ba353d
AN3O <mostproduct@gmail.com>
2024-08-15 13:16:08 +0000

    - Added EOS SDK 1.16.3-CL35276460 support - Using tokens for maven publish

--- eossdk/src/main/java/host/anzo/eossdk/eos/sdk/anticheat/common/enums/EOS_EAntiCheatCommonPlayerTakeDamageResult.java
+++ eossdk/src/main/java/host/anzo/eossdk/eos/sdk/anticheat/common/enums/EOS_EAntiCheatCommonPlayerTakeDamageResult.java
@@ -22,10 +22,16 @@ import java.util.Map;
 public @Getter enum EOS_EAntiCheatCommonPlayerTakeDamageResult implements NativeMapped {
 	/** No direct state change consequence for the victim */
 	EOS_ACCPTDR_None(0),
-	/** Player character is temporarily incapacitated and requires assistance to recover */
-	EOS_ACCPTDR_Downed(1),
-	/** Player character is permanently incapacitated and cannot recover (e.g. dead) */
-	EOS_ACCPTDR_Eliminated(2);
+	/** Deprecated - use more specific values below instead */
+	EOS_ACCPTDR_Downed_DEPRECATED(1),
+	/** Deprecated - use more specific values below instead */
+	EOS_ACCPTDR_Eliminated_DEPRECATED(2),
+	/** Player character transitioned from a normal state to temporarily incapacitated and requires assistance to recover. */
+	EOS_ACCPTDR_NormalToDowned(3),
+	/** Player character transitioned from a normal state to permanently incapacitated and cannot recover (e.g. dead). */
+	EOS_ACCPTDR_NormalToEliminated(4),
+	/** Player character transitioned from a temporarily incapacitated state to permanently incapacitated and cannot recover (e.g. dead). */
+	EOS_ACCPTDR_DownedToEliminated(5);
 
 	private final int id;
 
